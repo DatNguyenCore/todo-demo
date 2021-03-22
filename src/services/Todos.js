3527034df02c4a335cc28a9af6ce6a2b6a862794
DatNguyen <dat>
2021-03-22 22:18:20 +0700

    [add] init project

---
+++ src/services/Todos.js
@@ -0,0 +1,21 @@
+const Todos = [
+    {
+        id: 1,
+        remark: 'Thực hành code Android.',
+        timeCreate: '13:20 1/3/2021',
+        list: [
+            'Chuẩn bị môi trường phát triển',
+            'Chuẩn bị máy tính'
+        ]
+    },
+    {
+        id: 2,
+        remark: 'Học cấu trúc dữ liệu và giải thuật.',
+        timeCreate: '16:20 1/3/2021',
+        list: [
+            'Viết thuật toán sắp xếp bằng C'
+        ]
+    },
+];
+
+export default Todos;
---
+++ src/services/index.js
@@ -0,0 +1,9 @@
+import Genres from './Genres';
+import Products from './Products';
+import Todos from './Todos';
+
+export {
+    Genres,
+    Products,
+    Todos
+}
